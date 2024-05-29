079e03e284e533f93cd5efe0ba74fa5e62ac35ba
Quế Nguyệt <quentn0620@gmail.com>
2024-05-29 14:25:57 +0700

    update search features: use dynamic linq

--- backend/Helpers/SearchHelper.cs
+++ backend/Helpers/SearchHelper.cs
@@ -1,89 +1,54 @@
-﻿using System.Linq;
-using System.Text.RegularExpressions;
-using backend.Models;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Linq.Dynamic.Core;
+using backend.Models;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace backend.Helpers
 {
     public class SearchHelper
     {
+        private string GenerateCondition(string column)
+        {
+            char[] punctuationMarks = { ' ', '-', ':', ',', '.', '?', '!' };
+            string processedColumn = $"{column}.ToString().ToLower()"; 
+            string condition = $"{processedColumn}.Equals(@0) || " +
+                               $"{processedColumn}.StartsWith(@0 + \" \") || " +
+                               $"{processedColumn}.EndsWith(\" \" + @0)";
+
+            foreach (char punctuationMark in punctuationMarks)
+            {
+                condition += $" || {processedColumn}.Contains(\" \" + @0 + \"{punctuationMark}\")";
+            }
+
+            return condition;
+        }
+
         public IQueryable<Product> ApplyProductSearchFilter(IQueryable<Product> query, string search, string searchColumn)
         {
-            search = search.ToLower();
-            switch (searchColumn.ToLower())
-            {
-                case "id":
-                    return query.Where(p => p.Id.ToString().Equals(search));
-                case "code":
-                    return query.Where(p => p.Code.ToLower().Equals(search) ||
-                                            p.Code.ToLower().StartsWith($"{search} ") ||
-                                            p.Code.ToLower().EndsWith($" {search}"));
-                case "name":
-                    return query.Where(p => p.Name.ToLower().Equals(search) ||
-                                            p.Name.ToLower().Contains($" {search} ") ||
-                                            p.Name.ToLower().StartsWith($"{search} ") ||
-                                            p.Name.ToLower().EndsWith($" {search}"));
-                case "category":
-                    return query.Where(p => p.Category.ToLower().Equals(search) ||
-                                            p.Category.ToLower().Contains($" {search} ") ||
-                                            p.Category.ToLower().StartsWith($"{search} ") ||
-                                            p.Category.ToLower().EndsWith($" {search}"));
-                case "brand":
-                    return query.Where(p => p.Brand != null &&
-                                            (p.Brand.ToLower().Equals(search) ||
-                                            p.Brand.ToLower().Contains($" {search} ") ||
-                                            p.Brand.ToLower().StartsWith($"{search} ") ||
-                                            p.Brand.ToLower().EndsWith($" {search}")));
-                case "type":
-                    return query.Where(p => p.Type != null &&
-                                            (p.Type.ToLower().Equals(search) ||
-                                            p.Type.ToLower().Contains($" {search} ") ||
-                                            p.Type.ToLower().StartsWith($"{search} ") ||
-                                            p.Type.ToLower().EndsWith($" {search}")));
-                case "description":
-                    return query.Where(p => p.Description != null &&
-                                (p.Description.ToLower() == search ||
-                                 p.Description.ToLower().Contains($" {search} ") ||
-                                 p.Description.ToLower().StartsWith($"{search} ") ||
-                                 p.Description.ToLower().EndsWith($" {search}") ||
-                                 p.Description.ToLower().Contains($" {search}-") ||
-                                 p.Description.ToLower().Contains($" {search}:") ||
-                                 p.Description.ToLower().Contains($" {search},") ||
-                                 p.Description.ToLower().Contains($" {search}.") ||
-                                 p.Description.ToLower().Contains($" {search}?") ||
-                                 p.Description.ToLower().Contains($" {search}!")));
+            List<string> validColumns = new List<string> { "Id", "Code", "Name", "Category", "Brand", "Type", "Description" };
+            string formattedSearch = search.ToLower();
 
-                default:
-                    return query.Where(p => p.Id.ToString().Equals(search) ||
-                                            p.Code.ToLower().Equals(search) ||
-                                            p.Code.ToLower().StartsWith($"{search} ") ||
-                                            p.Code.ToLower().EndsWith($" {search}") ||
-                                            p.Name.ToLower().Equals(search) ||
-                                            p.Name.ToLower().Contains($" {search} ") ||
-                                            p.Name.ToLower().StartsWith($"{search} ") ||
-                                            p.Name.ToLower().EndsWith($" {search}") ||
-                                            p.Category.ToLower().Equals(search) ||
-                                            p.Category.ToLower().Contains($" {search} ") ||
-                                            p.Category.ToLower().StartsWith($"{search} ") ||
-                                            p.Category.ToLower().EndsWith($" {search}") ||
-                                            (p.Brand != null && (p.Brand.ToLower().Equals(search) ||
-                                                                p.Brand.ToLower().Contains($" {search} ") ||
-                                                                p.Brand.ToLower().StartsWith($"{search} ") ||
-                                                                p.Brand.ToLower().EndsWith($" {search}"))) ||
-                                            (p.Type != null && (p.Type.ToLower().Equals(search) ||
-                                                                p.Type.ToLower().Contains($" {search} ") ||
-                                                                p.Type.ToLower().StartsWith($"{search} ") ||
-                                                                p.Type.ToLower().EndsWith($" {search}"))) ||
-                                            (p.Description != null && (p.Description.ToLower() == search ||
-                                                                    p.Description.ToLower().Contains($" {search} ") ||
-                                                                    p.Description.ToLower().StartsWith($"{search} ") ||
-                                                                    p.Description.ToLower().EndsWith($" {search}") ||
-                                                                    p.Description.ToLower().Contains($" {search}-") ||
-                                                                    p.Description.ToLower().Contains($" {search}:") ||
-                                                                    p.Description.ToLower().Contains($" {search},") ||
-                                                                    p.Description.ToLower().Contains($" {search}.") ||
-                                                                    p.Description.ToLower().Contains($" {search}?") ||
-                                                                    p.Description.ToLower().Contains($" {search}!"))));
+            if (string.IsNullOrEmpty(searchColumn) || searchColumn.Equals("all", StringComparison.OrdinalIgnoreCase))
+            {
+                List<string> conditions = new List<string>();
+
+                foreach (string column in validColumns)
+                {
+                    conditions.Add(GenerateCondition(column));
+                }
+
+                string finalCondition = string.Join(" || ", conditions);
+                return query.Where(finalCondition, formattedSearch);
+            }
+            else if (validColumns.Contains(searchColumn, StringComparer.OrdinalIgnoreCase))
+            {
+                return query.Where(GenerateCondition(searchColumn), formattedSearch);
+            }
+            else
+            {
+                throw new ArgumentException("Invalid search column");
             }
         }
     }
